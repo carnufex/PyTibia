ee75bf0f7bf57c93a9cd97b2e1f2558545139ad8
Christopher Rosenvall <christopher.rosenvall@dataductus.se>
2023-10-19 22:50:57 +0200

    tests

---
+++ test.py
@@ -0,0 +1,100 @@
+import time
+import cv2
+import dxcam
+import numpy as np
+from typing import Callable, Union
+import xxhash
+# from src.shared.typings import BBox, GrayImage
+import win32gui
+import win32ui
+from PIL import Image
+from ctypes import windll
+import pathlib
+from src.repositories.chat.core import getTabs, hasNewLoot, getLootLines
+from src.utils.image import loadFromRGBToGray
+
+
+
+
+
+camera = dxcam.create(output_color='BGRA')
+latestScreenshot = None
+
+
+
+def region_grabber():
+    start = time.time()
+    hwnd = win32gui.FindWindow(None, 'Fullscreen Projector (Preview)')
+
+
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w = int(right - left)
+    h = int(bot - top)
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+    saveDC.SelectObject(saveBitMap)
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area.
+    #result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 2)
+    # print(result)
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im = Image.frombuffer('RGB',
+        (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+        bmpstr, 'raw', 'BGRX', 0, 1)
+
+    # Convert the PIL image to a NumPy array
+    # im = np.frombuffer(bmpstr, dtype='uint8')
+    # im = im.reshape((bmpinfo['bmHeight'], bmpinfo['bmWidth'], 4))
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    if result == 1:
+        #PrintWindow Succeeded
+        # Save the image as a PNG file
+        im.save("test.png")
+        end = time.time()
+        #print("TIME: ", end - start)
+        im = np.array(im)
+        return im
+    else:
+        print("Something went shit in region_grabber()")
+
+
+# TODO: add unit tests
+def getScreenshot():
+    global camera, latestScreenshot
+    screenshot = region_grabber()
+    if screenshot is None:
+        return latestScreenshot
+    latestScreenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGRA2GRAY)
+    return latestScreenshot
+
+
+
+def testTabs():
+    currentPath = pathlib.Path(__file__).parent.resolve()
+    screenshot = loadFromRGBToGray(f'{currentPath}/screenshot.png')
+    x = getLootLines(screenshot)
+    print(x)
+
+
+# testTabs()
+
+
+data = [{'slot': (2, 3)},{'slot': (2, 3)},{'slot': (2, 3)}]
+creaturesSlots = [(y, x) for item in data for x, y in [item['slot']]]
+print(creaturesSlots[:,0])
